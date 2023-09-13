733134227b261439a589c32c72d78c3d06b72d46
Joseph Valdez <Joseph.W.Valdez@GMail.com>
2023-09-13 23:09:18 +0000

    submitting form now retrieves data from the API. added in basic styling

--- mortal-klimate/src/app/components/Form.tsx
+++ mortal-klimate/src/app/components/Form.tsx
@@ -1,42 +1,42 @@
 'use client';
-import { useEffect, useState } from 'react';
-
-const weatherApi = process.env.WEATHER_API_KEY
-const weatherApiUrl = process.env.WEATHER_API_URL
-async function fetchWeatherData() {
-  try {
-    const fetchUrl = `${weatherApiUrl}/current.json?key=${weatherApi}&q=London&aqi=no`
-    const response = await fetch(fetchUrl);
-
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-
-    const weatherData = await response.json();
-    // Handle and use the weather data here
-    console.log(weatherData);
-  } catch (error) {
-    console.error('Error fetching weather data:', error);
-  }
-}
+import { useState } from 'react';
 
 export default function Form() {
-    const [userLocation, setUserLocation] = useState('');
+  const [userLocation, setUserLocation] = useState('');
+  const weatherApi = process.env.NEXT_PUBLIC_WEATHER_API_KEY
+  const weatherApiUrl = process.env.NEXT_PUBLIC_WEATHER_API_URL
+
+  async function fetchWeatherData() {
+    try {
+      const fetchUrl = `${weatherApiUrl}/current.json?key=${weatherApi}&q=${userLocation}&aqi=no`
+      console.log('fetch', fetchUrl)
+      const response = await fetch(fetchUrl);
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const weatherData = await response.json();
+      console.log('weather results:', weatherData);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  }
 
     function handleSubmit(event: any) {
         event.preventDefault()
-        const formData = new FormData(event.currentTarget);
-        const location = formData.get('location')
-        console.log('Form Data', formData);
-        console.log('location', location);
+        fetchWeatherData()
         setUserLocation('');
     };
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form
+          onSubmit={handleSubmit}
+          className='flex gap-4'
+        >
             <input
                 id='location-input'
-                className='text-black'
+                className='text-black rounded-lg pl-4'
                 name='location'
                 value={userLocation}
                 type='text'
@@ -45,8 +45,9 @@ export default function Form() {
                 onChange={(e) => {setUserLocation(e.target.value)}}
             />
             <button
-                id='submit-button' 
+                id='submit-button'
                 type='submit'
+                className='bg-gray-300 px-2 py-1 rounded-lg text-black hover:bg-gray-500 hover:text-white transition duration-300'
             >
                     Submit
             </button>
--- mortal-klimate/src/app/page.tsx
+++ mortal-klimate/src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image'
 import Form from './components/Form'
 
 export default function Home() {
   return (
-      <main>
+      <main className='flex justify-center items-center w-screen h-screen'>
         <Form />
       </main>
   )
