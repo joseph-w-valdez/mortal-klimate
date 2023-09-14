66c69ddc1fc786c79fb6c22f93b0a20ccf975db0
Joseph Valdez <Joseph.W.Valdez@GMail.com>
2023-09-14 01:29:19 +0000

    added custom font

--- mortal-klimate/src/app/page.tsx
+++ mortal-klimate/src/app/page.tsx
@@ -3,7 +3,7 @@ import Form from './components/Form'
 export default function Home() {
   return (
       <main className='flex flex-col gap-4 justify-center items-center w-screen h-screen'>
-        <h1>MORTAL KLIMATE!!!!</h1>
+        <h1 className='mk text-5xl'>MORTAL KLIMATE!!!!</h1>
         <Form />
       </main>
   )
